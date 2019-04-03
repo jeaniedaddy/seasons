0362a13fcb33ae29bb48e1099becfbd40d77ef5d
steve <jeaniedaddy@gmail.com>
2019-04-03 13:07:59 -0400

    add class component

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SeasonDisplay = (props)=> {
+    return (
+        <div>Season Display
+            {props.children}
+        </div>
+    );
+};
+
+export default SeasonDisplay; 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//functional component
+// const App = () => {
+//     navigator.geolocation.getCurrentPosition(
+//         (location)=> console.log(location),
+//         (err)=> console.log(err)
+//     );
+//     return (
+//         <SeasonDisplay>
+//             <div>Hi there!</div>
+//         </SeasonDisplay>
+//     );
+// };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//class component
+class App extends React.Component {
+    render(){
+        navigator.geolocation.getCurrentPosition(
+            (location)=> console.log(location),
+            (err)=> console.log(err)
+        );
+        return <div>latitude:1 </div>
+
+    }
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
